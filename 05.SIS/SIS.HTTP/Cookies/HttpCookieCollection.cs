cfe7ec1d3343882248f7a781314308a9447391b0
ShadyObeyd <obeyd.shady@gmail.com>
2018-10-04 17:21:07 +0300

    Added Cookies And Sessions

---
+++ 05.SIS/SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,73 @@
+﻿namespace SIS.HTTP.Cookies
+{
+    using Contracts;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private const string CookieNullMessage = "Cookie cannot be null!";
+        private const string CookieIsContainedMessage = "Cannot add existing cookie!";
+        private const string CookieParametersNullMessage = "Cookie key and value cannot be null or empty!";
+        private const string CookieKeyNullMessage = "Given key cannot be null or empty!";
+
+        private readonly Dictionary<string, HttpCookie> cookies;
+
+        public HttpCookieCollection()
+        {
+            this.cookies = new Dictionary<string, HttpCookie>();
+        }
+
+        public void Add(HttpCookie cookie)
+        {
+            if (cookie == null)
+            {
+                throw new ArgumentException(CookieNullMessage);
+            }
+
+            if (string.IsNullOrEmpty(cookie.Key) || string.IsNullOrEmpty(cookie.Value))
+            {
+                throw new ArgumentException(CookieParametersNullMessage);
+            }
+
+            if (cookies.ContainsKey(cookie.Key))
+            {
+                throw new InvalidOperationException(CookieIsContainedMessage);
+            }
+
+            this.cookies.Add(cookie.Key, cookie);
+        }
+
+        public bool ContainsCookie(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException(CookieKeyNullMessage);
+            }
+
+            return this.cookies.ContainsKey(key);
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentException(CookieKeyNullMessage);
+            }
+
+            return this.cookies.FirstOrDefault(c => c.Key == key).Value;
+        }
+
+        public bool HasCookies()
+        {
+            return this.cookies.Any();
+        }
+
+        public override string ToString()
+        {
+            return string.Join("; ", this.cookies.Values);
+        }
+    }
+}
---
+++ 05.SIS/SIS.HTTP/Sessions/HttpSession.cs
@@ -0,0 +1,76 @@
+﻿namespace SIS.HTTP.Sessions
+{
+    using Contracts;
+
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class HttpSession : IHttpSession
+    {
+        private const string IdNullMessage = "Id cannot be null or empty!";
+        private const string NameNullMessage = "Name cannot be null or empty!";
+        private const string ParameterNullMessage = "Parameter cannot be null!";
+        private const string SessionIsContainedMessage = "Cannot add existing session!";
+
+        private readonly Dictionary<string, object> sessionParameters;
+
+        public HttpSession(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentException(IdNullMessage);
+            }
+
+            this.Id = id;
+            this.sessionParameters = new Dictionary<string, object>();
+        }
+
+        public string Id { get; }
+
+        public void AddParameter(string name, object parameter)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException(NameNullMessage);
+            }
+
+            if (parameter == null)
+            {
+                throw new ArgumentException(ParameterNullMessage);
+            }
+
+            if (this.sessionParameters.ContainsKey(name))
+            {
+                throw new InvalidOperationException(SessionIsContainedMessage);
+            }
+
+            this.sessionParameters.Add(name, parameter);
+        }
+
+        public void ClearParameters()
+        {
+            this.sessionParameters.Clear();
+        }
+
+        public bool ContainsParameter(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException(NameNullMessage);
+            }
+
+            return this.sessionParameters.ContainsKey(name);
+        }
+
+        public object GetParameter(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException(NameNullMessage);
+            }
+
+            return this.sessionParameters.FirstOrDefault(s => s.Key == name).Value;
+        }
+    }
+}
