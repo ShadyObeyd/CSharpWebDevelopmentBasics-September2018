61c6779e3415b9976a59a465005c1fa1852bdd60
ShadyObeyd <obeyd.shady@gmail.com>
2018-10-12 17:05:16 +0300

    Finished with AlbumsController.cs and all.html view in IRunes.App Project.

--- 05.SIS/IRunes.App/Controllers/AlbumsController.cs
+++ 05.SIS/IRunes.App/Controllers/AlbumsController.cs
@@ -1,6 +1,5 @@
 ﻿namespace IRunes.App.Controllers
 {
-    using Controllers;
     using SIS.HTTP.Requests.Contracts;
     using SIS.HTTP.Responses.Contracts;
     using SIS.WebServer.Results;
@@ -8,6 +7,8 @@
 
     public class AlbumsController : BaseController
     {
+        private const string NoAlbumsMessage = "There are currently no albums.";
+
         public IHttpResponse All(IHttpRequest request)
         {
             if (!this.IsAuthenticated(request))
@@ -17,14 +18,21 @@
 
             var albums = this.Context.Albums;
 
-            var albumsList = string.Empty;
-
             if (albums.Any())
             {
+                var albumsList = string.Empty;
+
                 foreach (var album in albums)
                 {
-                    // TODO concat album string!
+                    string albumHtml = $@"<p><a href=""/albums/details/{album.Id}"">{album.Name}</a></p>";
+
+                    albumsList += albumHtml;
+                    this.ViewBag["albumsList"] = albumsList;
                 }
+            }
+            else
+            {
+                this.ViewBag["albumsList"] = NoAlbumsMessage;
             }
 
             return this.View();
